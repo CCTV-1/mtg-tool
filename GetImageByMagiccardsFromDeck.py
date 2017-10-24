f5271dbc17a17e14adf2a1c670b9d9d85bba20d0
CCTV-1 <www.a327380135@qq.com>
2017-10-24 21:55:02 +0800

    use logging module replace print

--- GetImageByMagiccardsFromDeck.py
+++ GetImageByMagiccardsFromDeck.py
@@ -4,13 +4,14 @@
 """Get deck card image in https://magiccards.info/
 image info in https://magiccards.info/query?q=!{cardname}&v=card&s=cname"""
 
+import logging
 import os
-import sys
 import re
 from multiprocessing import Pool
 
 import requests
 from bs4 import BeautifulSoup
+
 
 def get_image_url(cardname):
     """Get card image url"""
@@ -22,12 +23,11 @@ def get_image_url(cardname):
         soup = BeautifulSoup(html, 'html.parser')
         image = soup.find('img', {'style': 'border: 1px solid black;'})
         cardurl = "https://magiccards.info{0}".format(image['src'])
+
     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
-        print("\nTimeOutError:\n\tGet card:{0} url time out".format(
-            cardname))
+        logging.info("Get card:%s url time out", cardname)
     except (AttributeError, TypeError):
-        print("\tcard {0} url not find\n".format(
-            cardname), file=sys.stderr)
+        logging.info("Card %s url not find\n", cardname)
     return cardurl
 
 def get_cardinfo(filename):
@@ -55,35 +55,32 @@ def downloadimage(cardname, cardurl):
         if 'image' in imageobject.headers['Content-Type']:
             # x mode in python3.3+
             open(cardname + '.full.jpg', 'xb').write(imageobject.content)
-            print("Download card:{0} success,the url is:{1}".format
-                  (cardname, cardurl))
+            logging.info("Download card:%s success,the url is:%s", cardname, cardurl)
         else:
-            print("\nContent-Type Error:\n\trequest type not is image\
-                ,the card is:{0},number is:{1}\n".format(cardname, cardurl), file=sys.stderr)
+            logging.info("Request type not is image,the card is:\
+                %s,number is:%s\n", cardname, cardurl)
     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
-        print("\nTimeOutError:\n\tDownload Card {0} request timeout stop downloading!\n".format(
-            cardname), file=sys.stderr)
+        logging.info("Download Card %s request timeout stop downloading!\n", cardname)
     except (AttributeError, TypeError, KeyError):
-        print("\nThe card:{0} information obtained is wrong\n".format(
-            cardname), file=sys.stderr)
+        logging.info("The card:%s information obtained is wrong\n", cardname)
 
 def main():
     """main function"""
+    logging.basicConfig(filename='GetImage.log', filemode='w', level=logging.DEBUG)
     deckfilename = input('You plan download card image deck file name:')
     cardinfo = get_cardinfo(deckfilename)
     if os.path.exists('./images') is False:
         os.mkdir('./images')
     os.chdir('./images')
     processes = Pool(processes=4)
-    print("Download deck image start,Card total {0}".format
-          (len(cardinfo)))
+    logging.info("Download deck image start,Card total %d", len(cardinfo))
     for cardobj in cardinfo:
         processes.apply_async(downloadimage, args=(
             cardobj[0], cardobj[1], ))
         #downloadimage( setshortname , cardobj[0] , cardobj[1] )
     processes.close()
     processes.join()
-    print('deck all card image download end')
+    logging.info('deck all card image download end')
     os.chdir('./..')
 
 if __name__ == '__main__':
