03d69e9137d33e391e69e1494dba3f9a91bfe18e
CCTV-1 <script.tar.gz@gmail.com>
2018-09-03 12:17:06 +0800

    support download to xmage filename format(cookies card)

--- spider/GetImageByGathererWizarads.py
+++ spider/GetImageByGathererWizarads.py
@@ -17,7 +17,7 @@ def getcardsinfo(setlongname):
     try:
         cardinfo = []
         cardinfourl = 'http://gatherer.wizards.com/Pages/Search/'\
-        'Default.aspx?sort=cn+&set=[\"{0}\"]'.format(setlongname)
+            'Default.aspx?sort=cn+&set=[\"{0}\"]'.format(setlongname)
         resp = requests.Session().get(cardinfourl, timeout=13, cookies={
             'CardDatabaseSettings': '0=1&1=28&2=0&14=1&3=13&4=0&5=1&6=15\
             &7=0&8=1&9=1&10=18&11=7&12=8&15=1&16=0&13='})
@@ -43,7 +43,7 @@ def getcardsinfo(setlongname):
         exit(False)
 
 
-def downloadimage(cardname, cardid):
+def downloadimage(cardname, cardid, filename_format='forge'):
     """Download the card image represented by cardname
     Cardid is used to determine the picture link"""
     renamecount = 0
@@ -52,10 +52,11 @@ def downloadimage(cardname, cardid):
     flag = 8
     try:
         cardurl = 'http://gatherer.wizards.com/Handlers/'\
-        'Image.ashx?multiverseid={0}&type=card'.format(cardid)
+            'Image.ashx?multiverseid={0}&type=card'.format(cardid)
         imageobject = requests.get(cardurl, timeout=13)
     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
-        logging.info("Download Card %s request timeout stop downloading!\n", cardname)
+        logging.info(
+            "Download Card %s request timeout stop downloading!\n", cardname)
     try:
         if 'image' in imageobject.headers['Content-Type']:
             while flag:
@@ -63,22 +64,31 @@ def downloadimage(cardname, cardid):
                 try:
                     # x mode in python3.3+
                     open(cardname + '.full.jpg', 'xb').write(imageobject.content)
-                    logging.info("Download card:%s success,the card id is %s\n", cardname, cardid)
+                    logging.info(
+                        "Download card:%s success,the card id is %s\n", cardname, cardid)
                     break
+                except FileNotFoundError:
+                    if filename_format == 'forge':
+                        cardname = cardname.replace(' // ', '')
+                    elif filename_format == 'xmage':
+                        cardname = cardname.replace('//', '-')
+                    else:
+                        pass
                 except FileExistsError:
                     # rename base land
                     renamecount += 1
                     cardname = basecardname + str(renamecount)
         else:
-            logging.info("Request type not is image"\
-            ",the card is:%s,number is:%s\n", cardname, cardid)
+            logging.info("Request type not is image"
+                         ",the card is:%s,number is:%s\n", cardname, cardid)
     except (AttributeError, TypeError, KeyError):
         logging.info("The card:%s information obtained is wrong\n", cardname)
 
 
 def main():
     """main function"""
-    logging.basicConfig(filename='GetImage.log', filemode='w', level=logging.INFO)
+    logging.basicConfig(filename='GetImage.log',
+                        filemode='w', level=logging.INFO)
     setshortname = input('You plan download setshortname:')
     setlongname = input('You plan download setlongname:')
     if os.path.exists('./' + setshortname) is False:
@@ -86,7 +96,8 @@ def main():
     os.chdir('./' + setshortname)
     cardsinfo = getcardsinfo(setlongname)
     P = Pool(processes=4)
-    print("Download set:%s start,Card total %d".format(setshortname, len(cardsinfo)))
+    print("Download set:{0} start,Card total {1}".format(
+        setshortname, len(cardsinfo)))
     for cardobj in cardsinfo:
         P.apply_async(downloadimage, args=(cardobj[0], cardobj[1], ))
         #downloadimage( cardobj )
@@ -94,6 +105,7 @@ def main():
     P.join()
     print('Set {0} all card image download end'.format(setshortname))
     os.chdir('./..')
+
 
 if __name__ == '__main__':
     main()
