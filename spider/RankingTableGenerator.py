d19215f2587ab16cc884c091065556be724590a3
CCTV-1 <script.tar.gz@gmail.com>
2019-09-21 18:53:04 +0800

    support select table content lang

--- spider/RankingTableGenerator.py
+++ spider/RankingTableGenerator.py
@@ -4,15 +4,17 @@ import logging
 import requests
 from openpyxl import Workbook
 
-columnnames = ["中文名", "英语名", "类别", "异能", "稀有度", "攻防", "现开", "轮抓", "构筑"]
+columnnames = ["中文名", "英语名", "类别", "法术力费用",
+               "异能", "稀有度", "攻防", "现开", "轮抓", "构筑"]
 raritytranslation = {"common": "普通",
                      "uncommon": "非普通", "rare": "稀有", "mythic": "秘稀"}
 
 
 class Card:
-    def __init__(self, zh: str, en: str, type: str, text: str, rarity: str, pt_value: str):
+    def __init__(self, zh: str, en: str, mc: str, type: str, text: str, rarity: str, pt_value: str):
         self.zh_name = zh
         self.en_name = en
+        self.manacost = mc
         self.type_name = type
         self.text = text
         self.rarity = raritytranslation[rarity]
@@ -30,33 +32,61 @@ def write_xlsx(setcode: str, cards: list):
     for number in range(0, cards.__len__()):
         sheet.cell(number + 2, 1, cards[number].zh_name)
         sheet.cell(number + 2, 2, cards[number].en_name)
-        sheet.cell(number + 2, 3, cards[number].type_name)
-        sheet.cell(number + 2, 4, cards[number].text)
-        sheet.cell(number + 2, 5, cards[number].rarity)
-        sheet.cell(number + 2, 6, cards[number].pt)
-        sheet.cell(number + 2, 7, cards[number].sealed_ranking)
-        sheet.cell(number + 2, 8, cards[number].darft_ranking)
-        sheet.cell(number + 2, 9, cards[number].construct_ranking)
+        sheet.cell(number + 2, 3, cards[number].manacost)
+        sheet.cell(number + 2, 4, cards[number].type_name)
+        sheet.cell(number + 2, 5, cards[number].text)
+        sheet.cell(number + 2, 6, cards[number].rarity)
+        sheet.cell(number + 2, 7, cards[number].pt)
+        sheet.cell(number + 2, 8, cards[number].sealed_ranking)
+        sheet.cell(number + 2, 9, cards[number].darft_ranking)
+        sheet.cell(number + 2, 10, cards[number].construct_ranking)
 
     wb.save('{0}.xlsx'.format(setcode))
 
 
-def get_cards(setcode: str):
+def get_cards(setcode: str, lang='en'):
     cards = []
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
     try:
         resp = requests.get(
-            'https://api.scryfall.com/cards/search?q=s:{0}+lang:zhs'.format(setcode), timeout=60, headers=headers)
+            'https://api.scryfall.com/cards/search?q=s:{0}+lang:{1}'.format(setcode, lang), timeout=60, headers=headers)
         if resp.status_code != 200:
             return None
         info_content = resp.json()
         for cardinfo in info_content['data']:
-            card_pt = "{0}/{1}".format(cardinfo.get('power'),
-                                       cardinfo.get('toughness'))
-            card_text = cardinfo.get('printed_text', '')
-            cards.append(Card(cardinfo['printed_name'], cardinfo['name'],
-                              cardinfo['printed_type_line'], card_text, cardinfo['rarity'], card_pt))
+            if 'printed_name' in cardinfo:
+                locale_name = cardinfo['printed_name']
+            else:
+                locale_name = cardinfo['name']
+
+            en_name = cardinfo['name']
+
+            if 'printed_type_line' in cardinfo:
+                type_str = cardinfo['printed_type_line']
+            else:
+                type_str = cardinfo['type_line']
+
+            if 'mana_cost' in cardinfo:
+                mana_cost_str = cardinfo['mana_cost']
+            else:
+                mana_cost_str = None
+
+            if 'printed_text' in cardinfo:
+                card_text = cardinfo['printed_text']
+            elif 'oracle_text' in cardinfo:
+                card_text = cardinfo['oracle_text']
+            else:
+                card_text = None
+
+            if 'power' in cardinfo:
+                card_pt = "{0}/{1}".format(cardinfo['power'],
+                                           cardinfo['toughness'])
+            else:
+                card_pt = None
+
+            cards.append(Card(locale_name, en_name, type_str, mana_cost_str,
+                              card_text, cardinfo['rarity'], card_pt))
         has_more = info_content['has_more']
 
         while has_more != False:
@@ -64,21 +94,46 @@ def get_cards(setcode: str):
                 info_content.get('next_page'), timeout=60, headers=headers)
             info_content = resp.json()
             for cardinfo in info_content['data']:
-                card_pt = "{0}/{1}".format(cardinfo.get('power', None),
-                                           cardinfo.get('toughness', None))
-                card_text = cardinfo.get('printed_text', '')
-                cards.append(Card(cardinfo['printed_name'], cardinfo['name'],
-                                  cardinfo['printed_type_line'], card_text, cardinfo['rarity'], card_pt))
+                if 'printed_name' in cardinfo:
+                    locale_name = cardinfo['printed_name']
+                else:
+                    locale_name = cardinfo['name']
+
+                en_name = cardinfo['name']
+
+                if 'printed_type_line' in cardinfo:
+                    type_str = cardinfo['printed_type_line']
+                else:
+                    type_str = cardinfo['type_line']
+
+                if 'mana_cost' in cardinfo:
+                    mana_cost_str = cardinfo['mana_cost']
+                else:
+                    mana_cost_str = None
+
+                if 'printed_text' in cardinfo:
+                    card_text = cardinfo['printed_text']
+                elif 'oracle_text' in cardinfo:
+                    card_text = cardinfo['oracle_text']
+                else:
+                    card_text = None
+
+                if 'power' in cardinfo:
+                    card_pt = "{0}/{1}".format(cardinfo['power'],
+                                               cardinfo['toughness'])
+                else:
+                    card_pt = None
+
+                cards.append(Card(locale_name, en_name, type_str, mana_cost_str,
+                                  card_text, cardinfo['rarity'], card_pt))
             has_more = info_content['has_more']
 
         return cards
-    except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout) as e:
-        logging.info("queue set:'%s' card list time out,message:'%s'",
-                     setcode)
+    except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
+        logging.info("%s", str(cardinfo))
         return cards
     except (AttributeError, TypeError, KeyError):
-        logging.info("queue set:'%s' list format is wrong\n",
-                     setcode)
+        logging.info("%s", str(cardinfo))
         return cards
 
 
@@ -86,5 +141,5 @@ if __name__ == "__main__":
     logging.basicConfig(filename='generator.log',
                         filemode='w', level=logging.INFO)
     code = input()
-    cards = get_cards(code)
+    cards = get_cards(code, 'en')
     write_xlsx(code, cards)
