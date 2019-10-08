15e65cdbeeca83597733f341383aa7e4e1825d7c
CCTV-1 <script.tar.gz@gmail.com>
2019-10-08 21:45:39 +0800

    fix double face card translation

--- spider/RatingTableGenerator.py
+++ spider/RatingTableGenerator.py
@@ -1,4 +1,5 @@
 import json
+from enum import Enum
 
 import requests
 from openpyxl import Workbook
@@ -7,6 +8,20 @@ columnnames = ["编号", "中文名", "英语名", "类别", "法术力费用",
                "异能", "稀有度", "攻防", "现开", "轮抓", "构筑"]
 raritytranslation = {"common": "普通",
                      "uncommon": "非普通", "rare": "稀有", "mythic": "秘稀"}
+
+
+class Languages(Enum):
+    EN = 'en'
+    ES = 'es'
+    FR = 'fr'
+    DE = 'de'
+    IT = 'it'
+    PT = 'pt'
+    JS = 'ja'
+    KO = 'ko'
+    RU = 'ru'
+    ZHS = 'zhs'
+    ZHT = 'zht'
 
 
 class Card:
@@ -45,12 +60,12 @@ def write_xlsx(setcode: str, cards: list):
     wb.save('{0}.xlsx'.format(setcode))
 
 
-def get_cards(setcode: str, lang='en'):
+def get_cards(setcode: str, lang=Languages.EN):
     cards = []
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
-    resurl = 'https://api.scryfall.com/cards/search?q=s:{0}+lang:{1}'.format(
-        setcode, lang)
+    resurl = 'https://api.scryfall.com/cards/search?q=s:{0}+lang:{1}+is:booster'.format(
+        setcode, lang.value)
     try:
         while True:
             resp = requests.get(resurl, timeout=60, headers=headers)
@@ -60,10 +75,24 @@ def get_cards(setcode: str, lang='en'):
             for cardinfo in info_content['data']:
                 id = cardinfo['collector_number']
 
-                if 'printed_name' in cardinfo:
-                    locale_name = cardinfo['printed_name']
+                if 'card_faces' not in cardinfo:
+                    if 'printed_name' in cardinfo:
+                        locale_name = cardinfo['printed_name']
+                    else:
+                        locale_name = cardinfo['name']
                 else:
-                    locale_name = cardinfo['name']
+                    cardface = cardinfo['card_faces'][0]
+                    locale_name = ""
+                    if 'printed_name' in cardface:
+                        locale_name += cardface['printed_name']
+                    else:
+                        locale_name += cardface['name']
+                    locale_name += ' // '
+                    cardface = cardinfo['card_faces'][1]
+                    if 'printed_name' in cardface:
+                        locale_name += cardface['printed_name']
+                    else:
+                        locale_name += cardface['name']
 
                 en_name = cardinfo['name']
 
@@ -82,13 +111,40 @@ def get_cards(setcode: str, lang='en'):
                 elif 'oracle_text' in cardinfo:
                     card_text = cardinfo['oracle_text']
                 else:
-                    card_text = None
+                    if 'card_faces' not in cardinfo:
+                        card_text = ""
+                    else:
+                        cardface = cardinfo['card_faces'][0]
+                        card_text = ""
+                        if 'printed_text' in cardface:
+                            card_text += cardface['printed_text']
+                        elif 'oracle_text' in cardface:
+                            card_text += cardface['oracle_text']
+                        cardface = cardinfo['card_faces'][1]
+                        if card_text is not "":
+                            card_text += '\n\n\n\n'
+                        if 'printed_text' in cardface:
+                            card_text += cardface['printed_text']
+                        elif 'oracle_text' in cardface:
+                            card_text += cardface['oracle_text']
 
                 if 'power' in cardinfo:
                     card_pt = "{0}/{1}".format(cardinfo['power'],
                                                cardinfo['toughness'])
                 else:
-                    card_pt = None
+                    if 'card_faces' not in cardinfo:
+                        card_pt = None
+                    else:
+                        cardface = cardinfo['card_faces'][0]
+                        card_pt = ""
+                        if 'power' in cardface:
+                            card_pt += "{0}/{1}".format(cardface['power'],
+                                                        cardface['toughness'])
+                            card_pt += '\n'
+                        cardface = cardinfo['card_faces'][1]
+                        if 'power' in cardface:
+                            card_pt += "{0}/{1}".format(cardface['power'],
+                                                        cardface['toughness'])
 
                 cards.append(Card(id, locale_name, en_name, type_str, mana_cost_str,
                                   card_text, cardinfo['rarity'], card_pt))
@@ -112,5 +168,5 @@ def get_cards(setcode: str, lang='en'):
 
 if __name__ == "__main__":
     code = input()
-    cards = get_cards(code, 'en')
+    cards = get_cards(code, Languages.ZHS)
     write_xlsx(code, cards)
