4077f592800500de31be80438e1004097b80defc
CCTV-1 <script.tar.gz@gmail.com>
2019-10-26 11:34:31 +0800

    update translation

--- mtgtool_zh_CN.ts
+++ mtgtool_zh_CN.ts
@@ -4,72 +4,72 @@
 <context>
     <name>DownloadManager</name>
     <message>
-        <location filename="download_manager.cpp" line="363"/>
+        <location filename="download_manager.cpp" line="365"/>
         <source>common</source>
         <translation>普通</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="366"/>
+        <location filename="download_manager.cpp" line="368"/>
         <source>uncommon</source>
         <translation>非普通</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="369"/>
+        <location filename="download_manager.cpp" line="371"/>
         <source>rare</source>
         <translation>稀有</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="372"/>
+        <location filename="download_manager.cpp" line="374"/>
         <source>mythic</source>
         <translation>秘稀</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="377"/>
+        <location filename="download_manager.cpp" line="379"/>
         <source>zh_name</source>
         <translation>中文名</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="377"/>
+        <location filename="download_manager.cpp" line="379"/>
         <source>en_name</source>
         <translation>英文名</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="377"/>
+        <location filename="download_manager.cpp" line="379"/>
         <source>type</source>
         <translation>类别</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="377"/>
+        <location filename="download_manager.cpp" line="379"/>
         <source>set_id</source>
         <translation>编号</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="378"/>
+        <location filename="download_manager.cpp" line="380"/>
         <source>text</source>
         <translation>异能</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="378"/>
+        <location filename="download_manager.cpp" line="380"/>
         <source>rarity</source>
         <translation>稀有度</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="378"/>
+        <location filename="download_manager.cpp" line="380"/>
         <source>pt</source>
         <translation>攻防</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="378"/>
+        <location filename="download_manager.cpp" line="380"/>
         <source>sealed_rating</source>
         <translation>现开评分</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="379"/>
+        <location filename="download_manager.cpp" line="381"/>
         <source>darft_rating</source>
         <translation>轮抓评分</translation>
     </message>
     <message>
-        <location filename="download_manager.cpp" line="379"/>
+        <location filename="download_manager.cpp" line="381"/>
         <source>construct_rating</source>
         <translation>构筑评分</translation>
     </message>
@@ -89,17 +89,22 @@
 <context>
     <name>PreviewDialog</name>
     <message>
-        <location filename="PreviewDialog.qml" line="24"/>
+        <location filename="PreviewDialog.qml" line="27"/>
         <source>deck preview</source>
         <translation>套牌预览</translation>
     </message>
     <message>
-        <location filename="PreviewDialog.qml" line="54"/>
+        <location filename="PreviewDialog.qml" line="57"/>
+        <source>commander</source>
+        <translation>指挥官</translation>
+    </message>
+    <message>
+        <location filename="PreviewDialog.qml" line="174"/>
         <source>Main</source>
         <translation>主牌</translation>
     </message>
     <message>
-        <location filename="PreviewDialog.qml" line="181"/>
+        <location filename="PreviewDialog.qml" line="291"/>
         <source>Sideboard</source>
         <translation>备牌</translation>
     </message>
@@ -107,33 +112,33 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="enums.cpp" line="42"/>
+        <location filename="enums.cpp" line="45"/>
         <source>a transparent, rounded full card PNG</source>
         <translation>透明的的圆角PNG卡图</translation>
     </message>
     <message>
-        <location filename="enums.cpp" line="43"/>
+        <location filename="enums.cpp" line="47"/>
         <source>a full card image with the rounded corners and the majority of the border cropped off</source>
         <translation>带有圆角和大部分边框的裁剪卡图</translation>
     </message>
     <message>
-        <location filename="enums.cpp" line="44"/>
+        <location filename="enums.cpp" line="46"/>
         <source>a rectangular crop of the card’s art only</source>
         <oldsource>a rectangular crop of the card’s art only.</oldsource>
         <translation>用于看画的卡图</translation>
     </message>
     <message>
-        <location filename="enums.cpp" line="45"/>
+        <location filename="enums.cpp" line="44"/>
         <source>a large full card image</source>
         <translation>大尺寸的完整卡图</translation>
     </message>
     <message>
-        <location filename="enums.cpp" line="46"/>
+        <location filename="enums.cpp" line="43"/>
         <source>a medium-sized full card image</source>
         <translation>中等尺寸的完整卡图</translation>
     </message>
     <message>
-        <location filename="enums.cpp" line="47"/>
+        <location filename="enums.cpp" line="42"/>
         <source>a small full card image</source>
         <translation>小尺寸的完整卡图</translation>
     </message>
@@ -232,7 +237,7 @@
         <translation type="vanished">备牌</translation>
     </message>
     <message>
-        <location filename="main.qml" line="380"/>
+        <location filename="main.qml" line="396"/>
         <source>choose a deck file</source>
         <translation>选择一个套牌文件</translation>
     </message>
@@ -241,13 +246,13 @@
         <translation type="vanished">设置</translation>
     </message>
     <message>
-        <location filename="main.qml" line="92"/>
+        <location filename="main.qml" line="108"/>
         <source>filter role:</source>
         <oldsource>filter role name:</oldsource>
         <translation>筛选对象：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="128"/>
+        <location filename="main.qml" line="144"/>
         <source>image language:</source>
         <translation>图片语言：</translation>
     </message>
@@ -256,59 +261,59 @@
         <translation type="vanished">图片清晰度：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="164"/>
+        <location filename="main.qml" line="180"/>
         <source>image styles:</source>
         <translation>图片样式：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="200"/>
+        <location filename="main.qml" line="216"/>
         <source>image name format:</source>
         <translation>图片名格式：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="235"/>
+        <location filename="main.qml" line="251"/>
         <source>image cache directory:</source>
         <translation>图片缓存目录：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="250"/>
+        <location filename="main.qml" line="266"/>
         <source>choose download image cache directory</source>
         <oldsource>choose download image saved directory</oldsource>
         <translation>选择下载图片缓存目录</translation>
     </message>
     <message>
-        <location filename="main.qml" line="272"/>
+        <location filename="main.qml" line="288"/>
         <source>preivew image width:</source>
         <translation>预览图片宽度：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="300"/>
+        <location filename="main.qml" line="316"/>
         <source>preivew image height:</source>
         <translation>预览图片高度：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="328"/>
+        <location filename="main.qml" line="344"/>
         <source>sets views button function:</source>
         <translation>系列视图按钮功能：</translation>
     </message>
     <message>
-        <location filename="main.qml" line="341"/>
+        <location filename="main.qml" line="357"/>
         <source>download sets image</source>
         <translation>下载卡图</translation>
     </message>
     <message>
-        <location filename="main.qml" line="342"/>
+        <location filename="main.qml" line="358"/>
         <source>generator sets rating table</source>
         <oldsource>generator sets ranking table</oldsource>
         <translation>生成评分表</translation>
     </message>
     <message>
-        <location filename="main.qml" line="451"/>
+        <location filename="main.qml" line="467"/>
         <source>Preferences Setting</source>
         <translation>偏好设置</translation>
     </message>
     <message>
-        <location filename="main.qml" line="469"/>
+        <location filename="main.qml" line="485"/>
         <source>sets views</source>
         <translation>系列视图</translation>
     </message>
@@ -317,17 +322,17 @@
         <translation type="vanished">测试下载器</translation>
     </message>
     <message>
-        <location filename="main.qml" line="353"/>
+        <location filename="main.qml" line="369"/>
         <source>preview generator</source>
         <translation>预览生成器</translation>
     </message>
     <message>
-        <location filename="main.qml" line="365"/>
+        <location filename="main.qml" line="381"/>
         <source>parse clipboard</source>
         <translation>从剪切板解析</translation>
     </message>
     <message>
-        <location filename="main.qml" line="387"/>
+        <location filename="main.qml" line="403"/>
         <source>parse deck</source>
         <translation>从套牌文件解析</translation>
     </message>
@@ -340,7 +345,7 @@
         <translation type="vanished">系列图片下载器</translation>
     </message>
     <message>
-        <location filename="main.qml" line="479"/>
+        <location filename="main.qml" line="495"/>
         <source>enter filter role string(part or all,only support english,case sensitive)</source>
         <translation>输入用于筛选对象的字符串（部分或者全部，只支持英语，区分大小写）</translation>
     </message>
@@ -358,7 +363,7 @@
         <translation type="vanished">输入系列名(部分或全称，只支持英文)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="77"/>
+        <location filename="main.qml" line="93"/>
         <source>Download Preferences</source>
         <translation>下载偏好</translation>
     </message>
@@ -367,7 +372,7 @@
         <translation type="vanished">拖放测试</translation>
     </message>
     <message>
-        <location filename="main.qml" line="440"/>
+        <location filename="main.qml" line="456"/>
         <source>deck preview generator</source>
         <translation>套牌预览生成器</translation>
     </message>
